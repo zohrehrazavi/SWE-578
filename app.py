1f46f701e7182929f4dd3012d01a9e233266c0de
Zohreh Razavi <zohrehrazavi10@gmail.com>
2025-05-15 20:20:47 +0300

    Update application with working models and passing tests

--- app.py
+++ app.py
@@ -86,4 +86,4 @@ if __name__ == '__main__':
     os.makedirs('static', exist_ok=True)
     os.makedirs('templates', exist_ok=True)
     
-    app.run(host='0.0.0.0', port=8080, debug=True) 
+    app.run(host='0.0.0.0', port=8081, debug=True) 
